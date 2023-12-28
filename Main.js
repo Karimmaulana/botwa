2f1a7ffca0a7fbb8fb0cbd1598aca3c4b205486e
Karimmaulana <106829535+Karimmaulana@users.noreply.github.com>
2023-12-28 20:00:09 +0700

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,17 @@
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+  header.classList.toggle ("maman", window.scrollY > 120);
+});
+
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x")
+  navlist.classList.toggle("active")
+}
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x")
+  navlist.classList.remove("active")
+}
